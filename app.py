94bfce7ae5bced6391881d8bfac9679034aca5e0
asf2171 <asf2171@columbia.edu>
2015-10-10 13:32:46 -0400

    Week4_3 Forsberg

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
 
 import json
 import time
@@ -16,10 +18,10 @@ def index():
 
 @app.route("/getData/")
 def getData():
-	
-	client = pyorient.OrientDB("localhost", 2424)
-	session_id = client.connect("root", "password")
-	db_name = "property_test"
+    
+        client = pyorient.OrientDB("localhost", 2424)
+	session_id = client.connect("root", "root")
+	db_name = "soufun"
 	db_username = "admin"
 	db_password = "admin"
 
@@ -30,21 +32,24 @@ def getData():
 		print "database [" + db_name + "] does not exist! session ending..."
 		sys.exit()
 		
-	lat1 = 22.532498
-	lat2 = 22.552317
-
-	lng1 = 114.044329
-	lng2 = 114.076644
-
+	lat1 = str(request.args.get('lat1'))
+        lng1 = str(request.args.get('lng1'))
+        lat2 = str(request.args.get('lat2'))
+        lng2 = str(request.args.get('lng2'))
+		
+		
 	query = 'SELECT FROM Listing WHERE latitude BETWEEN {} AND {} AND longitude BETWEEN {} AND {}'
-
-	records = client.command(query.format(lat1, lat2, lng1, lng2))
+        
+        records = client.command(query.format(lat1, lat2, lng1, lng2))
+        
+        numListings = len(records)
+        
+	print 'received ' + str(numListings) + ' records'
+	
+        print "received coordinates: [" + lat1 + ", " + lat2 + "], [" + lng1 + ", " + lng2 + "]"
 
 	random.shuffle(records)
 	records = records[:100]
-
-	numListings = len(records)
-	print 'received ' + str(numListings) + ' records'
 
 	client.db_close()
 
@@ -52,6 +57,8 @@ def getData():
 
 	for record in records:
 		feature = {"type":"Feature","properties":{},"geometry":{"type":"Point"}}
+
+	
 		feature["id"] = record._rid
 		feature["properties"]["name"] = record.title
 		feature["properties"]["price"] = record.price
@@ -60,6 +67,7 @@ def getData():
 		output["features"].append(feature)
 
 	return json.dumps(output)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
